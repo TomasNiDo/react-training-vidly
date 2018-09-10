a4cdf56ccae6cc3da42770a86c025f11b026a3d7
Thomas <thomasg@fligno.com.ph>
2018-09-10 15:23:22 +0800

    Extract the table element to Table component

--- src/components/MoviesTable.jsx
+++ src/components/MoviesTable.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Like from "./common/Like";
-import TableHeader from "./common/TableHeader";
-import TableBody from "./common/TableBody";
+import Table from "./common/Table";
 
 class MoviesTable extends Component {
   columns = [
@@ -32,14 +31,12 @@ class MoviesTable extends Component {
     const { movies, sortColumn, onSort } = this.props;
 
     return (
-      <table className="table table-hover">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        />
-        <TableBody data={movies} columns={this.columns} />
-      </table>
+      <Table
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
     );
   }
 }
