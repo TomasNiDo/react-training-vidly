6aa81842a1bcd7125a2534719094f055e36d1839
Thomas <thomasg@fligno.com.ph>
2018-09-11 16:39:38 +0800

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar'
 import Movies from './components/Movies'
+import Customers from './components/Customers'
+import Rentals from './components/Rentals'
+import NotFound from './components/NotFound'
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      <React.Fragment>
+        <Navbar />
+        <main className="container">
+          <Switch>
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
